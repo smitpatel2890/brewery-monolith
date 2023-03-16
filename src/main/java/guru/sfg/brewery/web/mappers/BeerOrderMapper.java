aa646575c8b4c26d6f4a46ba34080a3beae9e8ae
Smit Patel <smit.patel@hitachivantara.com>
2023-03-16 11:08:16 +0530

    add lombok-mapstruct-binding when using Lombok 1.18.16 and above

--- src/main/java/guru/sfg/brewery/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/sfg/brewery/web/mappers/BeerOrderMapper.java
@@ -21,6 +21,7 @@ import guru.sfg.brewery.domain.BeerOrder;
 import guru.sfg.brewery.web.model.BeerOrderDto;
 import org.mapstruct.Mapper;
 
+@Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
 
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
